b2552cb4f1d3cb7f90db38861e11ef35a2de231e
ivankp <ivan.pogrebnyak@gmail.com>
2021-06-22 23:24:51 -0400

    fixed merge script

--- run/merge.sh
+++ run/merge.sh
@@ -15,46 +15,53 @@ while [ "$path" != '/' ]; do
 done
 [ "$path" == '/' ] && err "cannot find bin/merge"
 
-mkdir -p "$2"
+mkdir -pv "$2"
 
 # merge runs --------------------------------------------------------
-for x in $(
-  find "$1" -name '*.root' \
-  | sed -nr 's,.*/(.*)_[0-9]+\.root$,\1,p' \
-  | sort -u
-); do
-  echo $x
-  out="$2/${x}.root"
-  in="$1/${x}_"
+find "$1" -name '*.root' |
+sed -nr 's|_[0-9]+\.root$||p' |
+sort -u |
+while read -r name; do
+  out="$(sed "s|^$1|$2|" <<< "$name").root"
+  echo "$out"
   m='y'
-  if [ -f "$out" ]; then
-    m=''
-    for f in "$in"*.root; do
-      if [ "$f" -nt "$out" ]; then
+  if [ -f "$out" ]; then # if output file exists
+    m='' # maybe skip
+    for f in "$name"*.root; do
+      if [ "$f" -nt "$out" ]; then # check if input was updated
         m='y'
         break
       fi
     done
   fi
-  [ ! -z "$m" ] && \
-    "$path/bin/merge" -ex "$out" "$in"*.root
+  if [ -n "$m" ]; then
+    # merge histograms
+    # scale to cross section (-x)
+    # produce scale and PDF variation envelopes (-e)
+    "$path/bin/merge" -ex "$out" "$name"*.root
+  fi
 done
 
 # merge NLO ---------------------------------------------------------
-for nlo in $(
-  find "$2" -name '*.root' \
-  | sed -nr 's,(^|/)([^_]+)(B|RS|I|V)_,\1NLO_,p' \
-  | sort -u
-); do
-  echo "$nlo" \
-  | sed -r 's,((^|(.*/))[^_]+)NLO(_.*),\1B\4\n\1RS\4\n\1I\4\n\1V\4,' \
-  | readarray parts
+find "$2" -name '*.root' |
+sed -nr 's,((^|/)[^_]*)(B|RS|I|V)_,\1NLO_,p' |
+sort -u |
+while read -r nlo; do
+  echo "$nlo"
+  readarray -t parts < <(
+    sed -r 's,(.*(^|/)[^_]*)NLO(_.*),\1B\3\n\1RS\3\n\1I\3\n\1V\3,' \
+    <<< "$nlo" )
   m=''
   for f in "${parts[@]}"; do
-    [ ! -f "$f" ] && continue 2
-    [ -f "$nlo" ] || [ "$f" -nt "$nlo" ] && m='y'
+    [ ! -r "$f" ] && continue 2 # skip if a part is missing
+    if [ ! -f "$nlo" ] || [ "$f" -nt "$nlo" ]; then
+      # don't skip if output doesn't exist or input was updated
+      m='y'
+    fi
   done
-  [ ! -z "$m" ] && \
+  if [ -n "$m" ]; then
+    # merge histograms
     "$path/bin/merge" "$nlo" "${parts[@]}"
+  fi
 done
 
